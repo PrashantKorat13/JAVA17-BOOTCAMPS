24f0e9cd923a43d01b9a4dcbd6e51e7c36f03989
PrashantKorat13 <prashant.korat13@gmail.com>
2023-02-10 20:57:51 +0000

    First commit for RandomNumber Programm

---
+++ Randomnuber.java
@@ -0,0 +1,11 @@
+package org.smvs1.JavaHW;
+
+import java.lang.Math;
+public class Randomnuber {
+
+    public static void main(String[] args) {
+
+        System.out.println("Random Number: " + Math.random());
+
+    }
+}
